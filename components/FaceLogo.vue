080107cabec1646ca33b73193f9b433efc8ed0af
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2022-07-06 01:37:56 +0900

    first commmit

---
+++ components/FaceLogo.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-card width="80%" height="auto" class="rounded-xl mx-auto">
+    <!-- <v-row no-gutters>
+      <v-col :cols="3">
+        <v-card class="pa-2" outlined tile> col-3 </v-card>
+        
+      </v-col>
+      <v-col :cols="9" :md="4">
+        <v-card class="pa-2" outlined tile> col-9 </v-card>
+        <v-card-text>{{ $attrs.name }}</v-card-text>
+      </v-col> -->
+    <!-- </v-row> -->
+    <v-row no-gutters align-content="center">
+      <v-col cols="4" align="center" class="py-5">
+        <h3>{{ $attrs.myName['myName'] }}</h3>
+        <v-list>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title
+                >趣味:{{ $attrs.myName['hobby'] }}</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+        <v-list>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title
+                >出身国:{{ $attrs.myName['country'] }}</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-col>
+      <v-col align="center" cols="8">
+        <v-img src="/my-face.png" class="my-5 my-face rounded-circle"> </v-img>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Vue, Prop } from 'nuxt-property-decorator'
+export default class FaceLogo extends Vue {
+  @Prop(String) readonly myName: string | undefined
+  age = 21
+}
+</script>
+
+<style>
+.my-face {
+  width: 200px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-container>
+    <v-row>
+      <FaceLogo :myName="profile" />
+    </v-row>
+    <v-row>
+      <v-card width="80%" height="auto" class="mx-auto my-3 rounded-xl">
+        <v-row>
+          <v-col cols="4" align="center">
+            <h2>スキル</h2>
+            <v-list>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>言語:JS,PHP,Python</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+            <v-list>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    DB:oracle,postgresql,mariadb</v-list-item-title
+                  >
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+            <v-list>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title> クラウド:GCP</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+            <v-list>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title> ver管理:svn,git</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-row>
+    <v-row>
+      <v-card width="80%" height="auto" class="mx-auto my-3 rounded-xl">
+        <v-row>
+          <v-col cols="4" align="center">
+            <h2>My Intro</h2>
+            <ul style="list-style: none">
+              <li>あだ名</li>
+              <p>タッキー</p>
+              <li>座右の銘</li>
+              <p>朝日は登らない</p>
+              <li>必殺技</li>
+              <p>寝る</p>
+            </ul></v-col
+          >
+        </v-row>
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import FaceLogo from '~/components/FaceLogo.vue'
+
+@Component({
+  components: {
+    FaceLogo,
+  },
+})
+export default class IndexPage extends Vue {
+  profile = {
+    myName: "TAKIGAWA's profile",
+    hobby: 'ゲーム',
+    country: '日本',
+  }
+}
+</script>
